55ba8da5526d807d9502d762bd7bfb8d0a0c4a9e
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2024-03-19 14:52:23 +0300

    работа 3 задание 4

--- lab3/task4.m
+++ lab3/task4.m
@@ -1,4 +1,4 @@
-% РѕРїРµСЂРґРµР»СЏРµРј РјР°С‚СЂРёС†Сѓ СЃРѕРµРґРёРЅРµРЅРёР№
+% оперделяем матрицу соединений
 % Na N C H O Ca
 A = [2 0 1 0 3 0; 
     0 1 0 1 3 0; 
@@ -7,74 +7,120 @@ A = [2 0 1 0 3 0;
     0 0 1 0 2 0;
     0 0 0 0 1 1; 
     0 2 0 0 6 1];
-A_tr = transpose(A)
-cond(A_tr(1:6, 2:6))
-r = rank(A)
-n = 7;
-k = n - r
-%Р°Р»РіРѕСЂРёС‚Рј РЅР°С…РѕР¶РґРµРЅРёСЏ РІСЃРµС… РЅРµРЅСѓР»РµРІС‹С… РїРѕРґРјР°С‚СЂРёС†
-rows = 1:n
-C = nchoosek(rows, k)
-D = []
-for i = 1:size(C)
-    rows_to_delete = C(i, :)
-    temp = A
-    temp(rows_to_delete, :) = []
-    [L, U, P] = lu(temp)
-    values = diag(U)
-    determinant = prod(values, 1)
-    if abs(determinant) > 0.0001
-	D = [D; rows_to_delete]
-    end
+ 
+ 
+syms Na N C H O Ca
+ 
+X = [Na; N; C; H; O; Ca];
+ 
+disp(A*X);
+mym = A*X;
+ 
+syms CNa2O3 HNO3 NaO3 H2O CO2 CaO;
+gh = [CNa2O3; HNO3; NaO3; H2O; CO2; CaO];
+
+syms Na2CO3 HN03 NaNO3 H2O CaO CO2 CaN2O6
+all_soed = [Na2CO3; HN03; NaNO3; H2O; CO2; CaO; CaN2O6];
+%disp(gh); 
+
+for i=1:6
+    qw = mym(i,:);
+    yt = gh(i);
+    %fprintf('%s = %s', char(qw), char(yt));
+    disp(' ');
+end    
+ 
+b = [  1 0 0 0 0;
+       0 0 1 0 0;
+       0 2 0 0 0;
+       1 0 0 0 2;
+       0 0 0 1 1;
+    ];
+
+b1 = [ 1 0 3 0 1 ;
+       0 0 1 2 0 ;
+       0 1 2 0 0 ;
+       0 0 1 0 0 ;
+       0 0 6 0 2 ;
+    ];
+
+M = [-2; -1; 0; 0; 0];
+
+ 
+t = [ 1 0 0 0 0 0 0];
+ 
+fprintf( '\nМатрица \n');
+disp(b);
+fprintf('________________________________________________________________\n');
+fprintf( 'Определитель матрицы: %d\n',det(b)); 
+fprintf( 'Норма матрицы: %d\n',norm(b)); 
+fprintf( 'Число обусловленности матрицы: %d\n',cond(b)); 
+fprintf( 'Вторая норма матриц: %d\n',norm(b,2)); 
+fprintf( 'Число обусловленности матрицы для второй нормы %d\n',cond(b,2)); 
+fprintf('________________________________________________________________\n\n');
+ 
+x = inv(b)*M;
+ 
+disp(t)
+for i=1:5
+    t(i+2) = x(i);
 end
-%Р·Р°РіСЂСѓР·РєР° РїР°РєРµС‚Р° СЃРёРјРІРѕР»РёРє
-echo off;
-pkg load symbolic;
-syms Na2(CO3) HNO3 NaNO3 H2O CO2 CaO Ca(NO3_2)
-M_all = [Na2(CO3), HNO3, NaNO3, H2O, CO2, CaO, Ca(NO3_2)]
-equations = []
-for l = 1:size(D)
-    M_basis = [Na2(CO3), HNO3, NaNO3, H2O, CO2, CaO, Ca(NO3_2)]
-    indexes = [D(l, 1); D(l, 2)]
-    n = size(indexes)
-    for i = 1:n
-        M_basis(indexes(i)) = 0
-    end
-    for i = 1:n
-        M_B = M_basis
-        M_B(indexes(i)) = 1 
-        res = zeros(1, columns(A))
-        [dict] = solve(M_B * A == res)
-        reagents = ""
-        products = ""
-        coef = 1
-        for val = key:dict
-            if (abs(val) < 1 && val > 0)
-            disp(val)
-                 coef = 1 / val
-            end
-        end
-        
-        for val = key:dict
-             val *= coef
-            if (val == 0)
-                continue
-            end
-            if (val > 0)
-                if(val == 1)
-                    products = strcat(products," + ", char(key))
-                else
-                    products = strcat(products," + ", char(val),"*", char(key))
-                end
-            else
-                reagents = strcat(reagents," + ", char(-val),"*", char(key))
-            end
-        end
-        if(coef == 1)
-            equation = strcat(reagents, " = ", products, " + ", char(M_all(indexes(i))))
-        else
-            equation = strcat(reagents, " = ", products, " + ", char(coef), "*", char(M_all(indexes(i))))
-        end
-        equations = [equations; equation]
-    end
+ 
+fprintf( 'Решение \n');
+disp(t)
+
+rez = t * all_soed;
+%disp(rez);
+fprintf('2*NaNC3 + CO2 + CaO = CaN2O6 + Na2CO3\n\n');
+
+fprintf( 'Проверка условия B*A=0\n');
+disp(t*A);
+ 
+ 
+fprintf( '\n____________________________________________________________________________________________________________________\n\n');
+ 
+b = [  1 0 0 0 0;
+       0 0 1 0 0;
+       0 2 0 0 0;
+       1 0 0 0 2;
+       0 0 0 1 1;
+    ];
+ 
+ 
+M = [0; 0; -1; -1; 0];
+ 
+t = [ 0 1 0 0 0 0 0];
+
+fprintf( '\nМатрица \n');
+disp(b);
+fprintf('________________________________________________________________\n');
+fprintf( 'Определитель матрицы: %d\n',det(b)); 
+fprintf( 'Норма матрицы: %d\n',norm(b)); 
+fprintf( 'Число обусловленности матрицы: %d\n',cond(b)); 
+fprintf( 'Вторая норма матриц: %d\n',norm(b,2)); 
+fprintf( 'Число обусловленности матрицы для второй нормы %d\n',cond(b,2)); 
+fprintf('________________________________________________________________\n\n');
+ 
+ 
+ 
+ 
+x = inv(b) * M;
+ 
+for i=1:5
+    t(i+2) = x(i);
 end
+ 
+ 
+fprintf( 'Решение \n');
+disp(t);
+
+rez = t * all_soed;
+%disp(rez);
+
+reag = [H H];
+prod = [H H];
+fprintf('CaN2O6 + H2O2 = 2HNO3 + CaO\n\n');
+
+fprintf( 'Проверка условия B*A=0\n');
+disp(t*A);
+
