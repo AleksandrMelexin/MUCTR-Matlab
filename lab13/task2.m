5746a9da477ede6593d90bd8cb44e0f9a5d201f3
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2024-05-29 20:18:46 +0300

    работа 13 доделана

--- lab13/task2.m
+++ lab13/task2.m
@@ -1,67 +1,129 @@
 clc; clear;
 
-% Эйлер явный
-x_explicit(1) = 0;
-y1_explicit(1) = 1;
-y2_explicit(1) = 1;
+% Параметры
+tol = 1e-3; % Увеличение допустимой погрешности для уменьшения частоты уменьшения шага
 h = 0.1;
+
+% Инициализация для явного метода Эйлера
+x_explicit = 0;
+y1_explicit = 1;
+y2_explicit = 1;
+
+% Массивы для хранения чисел жёсткости и шагов
+stiffness_numbers = [];
+steps = [];
+
 i = 1;
-disp('Числа жёсткости') 
-while(x_explicit(i) < 0.9) 
-    g = [exp(x_explicit(i).^2), x_explicit(i); -1, 2];
+while x_explicit(end) <= 1
+    % Матрица Якоби и число жёсткости
+    g = [exp(x_explicit(end)^2), x_explicit(end); -1, 2];
     s = max(real(eig(g))) / min(real(eig(g)));
-    fprintf('%d %d\n', i, s);
+    stiffness_numbers(end + 1) = s;
+
+    % Оценка ошибок для явного метода Эйлера
+    y1_temp = y1_explicit(end) + (y1_explicit(end) * exp(-x_explicit(end)^2) + x_explicit(end) * y2_explicit(end)) * h;
+    y2_temp = y2_explicit(end) + (3 * x_explicit(end) - y1_explicit(end) + 2 * y2_explicit(end)) * h;
+
+    y1_explicit_half = y1_explicit(end) + (y1_explicit(end) * exp(-x_explicit(end)^2) + x_explicit(end) * y2_explicit(end)) * (h / 2);
+    y2_explicit_half = y2_explicit(end) + (3 * x_explicit(end) - y1_explicit(end) + 2 * y2_explicit(end)) * (h / 2);
+
+    y1_explicit_double = y1_explicit_half + (y1_explicit_half * exp(-(x_explicit(end) + h / 2)^2) + (x_explicit(end) + h / 2) * y2_explicit_half) * (h / 2);
+    y2_explicit_double = y2_explicit_half + (3 * (x_explicit(end) + h / 2) - y1_explicit_half + 2 * y2_explicit_half) * (h / 2);
+
+    error1 = abs(y1_explicit_double - y1_temp);
+    error2 = abs(y2_explicit_double - y2_temp);
+    error = max(error1, error2);
+
+    if error > tol
+        h = h / 2;  % уменьшение шага
+    elseif error < tol / 4
+        h = h * 2;  % увеличение шага
+    end
+
+    % Принятие шага
+    y1_explicit(end + 1) = y1_temp;
+    y2_explicit(end + 1) = y2_temp;
+    x_explicit(end + 1) = x_explicit(end) + h;
+    
+    % Запись текущего шага в массив
+    steps(end + 1) = h;
     i = i + 1;
-    y1_explicit(i) = y1_explicit(i-1) + (y1_explicit(i-1) * exp(-x_explicit(i-1).^2) + x_explicit(i-1) * y2_explicit(i-1)) * h;
-    y2_explicit(i) = y2_explicit(i-1) + (3 * x_explicit(i-1) - y1_explicit(i-1) + 2 * y2_explicit(i-1)) * h;
-    x_explicit(i) = x_explicit(i-1) + h; 
 end
-fprintf('\n\n'); 
+
+% Вывод чисел жёсткости для явного йлера
+fprintf('Числа жёсткости:\n');
+for i = 1:length(stiffness_numbers)
+    fprintf('Итерация %d: число жёсткости = %f\n', i, stiffness_numbers(i));
+end
+
+fprintf('\n\n');
+% Вывод шагов для явного йлера
+fprintf('Шаги:\n');
+for i = 1:length(steps)
+    fprintf('Итерация %d: шаг = %f\n', i, steps(i));
+end
+
+fprintf('\n\n');
 % Вывод найденных значений функции
 fprintf('Значения функции y1 = f1(x) по явному методу Эйлера:\n\n');
 for i = 1:length(x_explicit)
     disp([num2str(x_explicit(i)), '   ', num2str(y1_explicit(i))]);
-    fprintf('\n');
 end
 fprintf('\n');
 fprintf('Значения функции y2 = f2(x) по явному методу Эйлера:\n\n');
 for i = 1:length(x_explicit)
     disp([num2str(x_explicit(i)), '   ', num2str(y2_explicit(i))]);
-    fprintf('\n');
 end
-disp('___________________________________________________________________') 
+disp('___________________________________________________________________')
 
-% метод неявного Эйлера 
-x_implicit(1) = 0;
-y1_implicit(1) = 1;
-y2_implicit(1) = 1;
-syms y1_sym y2_sym
-h = 0.1;
+% Инициализация для неявного метода Эйлера
+x_implicit = 0;
+y1_implicit = 1;
+y2_implicit = 1;
+
 i = 1;
 
-while(x_implicit(i) < 0.9)  
+while x_implicit(end) <= 1
+    % Матрица Якоби и число жёсткости
+    g = [exp(x_explicit(end)^2), x_explicit(end); -1, 2];
+    s = max(real(eig(g))) / min(real(eig(g)));
+    stiffness_numbers(end + 1) = s;
+
+    % Оценка ошибок для неявного метода Эйлера
+    y1_temp = y1_implicit(end) + (y1_implicit(end) * exp(x_implicit(end)^2) + x_implicit(end) * y2_implicit(end)) * h;
+    y2_temp = y2_implicit(end) + (3 * x_implicit(end) - y1_implicit(end) + 2 * y2_implicit(end)) * h;
+
+    y1_half = y1_implicit(end) + (y1_implicit(end) * exp(x_implicit(end)^2) + x_implicit(end) * y2_implicit(end) + y1_temp) * (h / 2);
+    y2_half = y2_implicit(end) + (3 * x_implicit(end) - y1_implicit(end) + 2 * y2_implicit(end) + 3 * x_implicit(end) - y1_temp + 2 * y2_temp) * (h / 2);
+
+    error1 = abs(y1_half - y1_temp);
+    error2 = abs(y2_half - y2_temp);
+    error = max(error1, error2);
+
+    if error > tol
+        h = h / 2;  % уменьшение шага
+    elseif error < tol / 4
+        h = h * 2;  % увеличение шага
+    end
+
+    % Принятие шага
+    y1_implicit(end + 1) = y1_temp;
+    y2_implicit(end + 1) = y2_temp;
+    x_implicit(end + 1) = x_implicit(end) + h;
+    
+    % Запись текущего шага в массив
+    steps(end + 1) = h;
     i = i + 1;
-    F1 = y1_sym == y1_implicit(i-1) + (y1_sym * exp(x_implicit(i-1).^2) + x_implicit(i-1) * y2_sym) * h;
-    F2 = y2_sym == y2_implicit(i-1) + (3 * x_implicit(i-1) - y1_sym + 2 * y2_sym) * h;
-    
-    S = vpasolve([F1, F2], [y1_sym, y2_sym], [y1_implicit(i-1), y2_implicit(i-1)]);
-    disp(S);
-    
-    y1_implicit(i) = double(S.y1_sym);
-    y2_implicit(i) = double(S.y2_sym);
-    x_implicit(i) = x_implicit(i-1) + h; 
 end
 
-% Вывод найденных значений функции 
+% Вывод найденных значений функции
 fprintf('Значения функции y1 = f1(x) по неявному методу Эйлера:\n\n');
 for i = 1:length(x_implicit)
     disp([num2str(x_implicit(i)), '   ', num2str(y1_implicit(i))]);
-    fprintf('\n');
 end
 fprintf('Значения функции y2 = f2(x) по неявному методу Эйлера:\n\n');
 for i = 1:length(x_implicit)
     disp([num2str(x_implicit(i)), '   ', num2str(y2_implicit(i))]);
-    fprintf('\n');
 end
 disp('___________________________________________________________________')
 
@@ -72,6 +134,18 @@ y0 = [1; 1];
 % Решение системы дифференциальных уравнений
 [x_ode15s, y_ode15s] = ode15s(@myODEs, [0, 1], y0);
 
+% Вывод чисел жёсткости для неявного Эйлера
+fprintf('Числа жёсткости:\n');
+for i = 1:length(stiffness_numbers)
+    fprintf('Итерация %d: число жёсткости = %f\n', i, stiffness_numbers(i));
+end
+
+% Вывод шагов для неявного йлера
+fprintf('Шаги:\n');
+for i = 1:length(steps)
+    fprintf('Итерация %d: шаг = %f\n', i, steps(i));
+end
+
 % Визуализация результатов
 figure
 hold on
@@ -79,8 +153,8 @@ plot(x_explicit, y1_explicit, 'r', 'DisplayName', 'Явный Эйлер y1');
 plot(x_explicit, y2_explicit, 'g', 'DisplayName', 'Явный Эйлер y2');
 plot(x_implicit, y1_implicit, 'b', 'DisplayName', 'Неявный Эйлер y1');
 plot(x_implicit, y2_implicit, 'm', 'DisplayName', 'Неявный Эйлер y2');
-plot(x_ode15s, y_ode15s(:,1), 'k--', 'DisplayName', 'MATLAB y1 (ODE15s)');
-plot(x_ode15s, y_ode15s(:,2), 'c--', 'DisplayName', 'MATLAB y2 (ODE15s)');
+plot(x_ode15s, y_ode15s(:, 1), 'k--', 'DisplayName', 'MATLAB y1 (ODE15s)');
+plot(x_ode15s, y_ode15s(:, 2), 'c--', 'DisplayName', 'MATLAB y2 (ODE15s)');
 grid on
 xlabel('x');
 ylabel('y');
@@ -90,5 +164,5 @@ hold off
 
 % Определение функции myODEs
 function dydx = myODEs(x, y)
-    dydx = [y(1) * exp(x.^2) + x * y(2); 3 * x - y(1) + 2 * y(2)];
+    dydx = [y(1) * exp(x^2) + x * y(2); 3 * x - y(1) + 2 * y(2)];
 end
